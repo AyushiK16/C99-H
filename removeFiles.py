43a9c9a99645297461e76f8dad55744aa56ac03e
AyushiK16 <62651426+AyushiK16@users.noreply.github.com>
2021-06-04 12:26:07 +0530

    Update removeFiles.py

--- removeFiles.py
+++ removeFiles.py
@@ -7,6 +7,39 @@ listOfFiles = os.listdir(folderPath)
 
 secondsInDay = 60*60*24
 seconds_30 = secondsInDay*30
+currentTime = time.time()
+
+
+if os.path.exists(folderPath):
+    os.walk(folderPath)
+    for root,folders,files in os.walk(folderPath):
+        ctime = os.stat(folderPath).st_ctime
+        if ctime <currentTime - seconds_30:
+            shutil.rmtree(folderPath)
+    
+        else:
+            for file in listOfFiles:
+                ctime = os.stat(folderPath + '/' + file).st_ctime
+                print(ctime, currentTime - seconds_30)
+
+                if ctime < currentTime - seconds_30:
+                    #print(ctime, seconds_30)
+                    #os.remove(folderPath + '/' + file)
+                    print(folderPath + '/' + file  + ' has been removed.')
+                else:
+                    print(folderPath + '/' + file  + ' is still applicable.')
+
+
+''' OLD CODE
+import os
+import shutil
+import time
+
+folderPath = input('Enter the folder path: ')
+listOfFiles = os.listdir(folderPath)
+
+secondsInDay = 60*60*24
+seconds_30 = secondsInDay*30
 
 
 if os.path.exists(folderPath):
@@ -19,6 +52,6 @@ if os.path.exists(folderPath):
         
         else:
             print(folderPath + '/' + file  + 'is still applicable.')
-
+'''
 
 
