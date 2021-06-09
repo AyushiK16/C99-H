ab4369641d247c1fa53f9c571b5bba4cb1cf9a71
AyushiK16 <62651426+AyushiK16@users.noreply.github.com>
2021-06-09 18:34:33 +0530

    Update removeFiles.py

--- removeFiles.py
+++ removeFiles.py
@@ -6,52 +6,63 @@ folderPath = input('Enter the folder path: ')
 listOfFiles = os.listdir(folderPath)
 
 secondsInDay = 60*60*24
-seconds_30 = secondsInDay*30
+#seconds_30 = secondsInDay*30
+seconds_30 = 30
 currentTime = time.time()
 
 
 if os.path.exists(folderPath):
-    os.walk(folderPath)
+    #os.walk(folderPath)
     for root,folders,files in os.walk(folderPath):
         ctime = os.stat(folderPath).st_ctime
         if ctime <currentTime - seconds_30:
             shutil.rmtree(folderPath)
     
         else:
-            for file in listOfFiles:
+            for file in files:
                 ctime = os.stat(folderPath + '/' + file).st_ctime
                 print(ctime, currentTime - seconds_30)
 
                 if ctime < currentTime - seconds_30:
                     #print(ctime, seconds_30)
-                    #os.remove(folderPath + '/' + file)
+                    os.remove(folderPath + '/' + file)
                     print(folderPath + '/' + file  + ' has been removed.')
                 else:
                     print(folderPath + '/' + file  + ' is still applicable.')
 
+            for folder in folders:
+                ctime = os.stat(folderPath + '/' + folder).st_ctime
 
-''' OLD CODE
-import os
-import shutil
-import time
-
-folderPath = input('Enter the folder path: ')
-listOfFiles = os.listdir(folderPath)
-
-secondsInDay = 60*60*24
-seconds_30 = secondsInDay*30
+                if ctime < currentTime - seconds_30:
+                    #print(ctime, seconds_30)
+                    shutil.rmtree(folderPath + '/' + folder)
+                    print(folderPath + '/' + folder  + ' has been removed.')
+                else:
+                    print(folderPath + '/' + folder  + ' is still applicable.')
 
 
-if os.path.exists(folderPath):
-    for file in listOfFiles:
-        ctime = os.stat(folderPath + '/' + file).st_ctime
-        if ctime > seconds_30:
-            print(ctime, seconds_30)
-            #os.remove(folderPath + '/' + file)
-            print(folderPath + '/' + file  + 'has been removed.')
-        
-        else:
-            print(folderPath + '/' + file  + 'is still applicable.')
+
+                #for folders
+
+
+
+
+
 '''
+os.walk(folderPath) - root, folders, files
+for root,folders,files in os.walk(folderPath):
+    - sep gives you all
+    check time for root folder, 
+        if true, delete the folder
+    shutil.rmtree(folderPath) => to delete a folder
 
+    else:
+        - check the folders 
+        - write the time condition for each folder
+        then shutil.rmtree(folderPath)
+
+        - check the files
+        - check for the condition, then os.remove (for files)       
+
+'''
 
